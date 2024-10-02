0bd20cba293b6b683fff210a9b58168360bef79a
Sneha Gupta <snehag0203@gmail.com>
2024-10-02 20:28:07 +0530

    update

---
+++ main.py
@@ -0,0 +1,10 @@
+def to_upper(name):
+    return name.upper()
+
+def say_hello(name):
+    print("hello", name)
+
+if __name__ == '__main__':
+    name="Sneha"
+    say_hello(name)
+    print(to_upper(name))
